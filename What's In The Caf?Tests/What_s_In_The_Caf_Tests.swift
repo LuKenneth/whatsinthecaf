81d592fcbd7e72b410e059224d1eba13ba88a9eb
Luke Patterson <Luke@lukes-mbp.campus.jcu.edu>
2015-10-07 14:35:51 -0400

    Initial Commit

---
+++ What's In The Caf?Tests/What_s_In_The_Caf_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  What_s_In_The_Caf_Tests.swift
+//  What's In The Caf?Tests
+//
+//  Created by Luke Patterson on 10/7/15.
+//  Copyright © 2015 Luke Patterson. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_In_The_Caf_
+
+class What_s_In_The_Caf_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
